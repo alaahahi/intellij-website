24adbecf1444604f711285293bae8f3babf828e1
alaahahi <alaa.hahe@gmail.com>
2025-12-15 02:21:29 +0300

    Fix dashboard route not found error

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,10 @@ require __DIR__.'/auth.php';
 
 // Authenticated Routes
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return redirect()->route('admin.dashboard');
+    })->name('dashboard');
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');

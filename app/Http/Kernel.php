715d051c08f1b8b0bb85d167e4053c818ebff90e
alaahahi <alaa.hahe@gmail.com>
2026-05-07 10:02:50 +0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\TrackVisit::class,
+            // \App\Http\Middleware\TrackVisit::class, // Temporarily disabled to stop custom visit logging
         ],
 
         'api' => [
